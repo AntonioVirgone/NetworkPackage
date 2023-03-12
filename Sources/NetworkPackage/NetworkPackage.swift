9ecd7cbcdd025a34e841514d6e6827e9f0690d05
antonio.virgone <antonio.virgone@wisemotions.com>
2023-03-12 18:09:38 +0100

    fix package

--- Sources/NetworkPackage/NetworkPackage.swift
+++ Sources/NetworkPackage/NetworkPackage.swift
@@ -1,6 +1,12 @@
+import Foundation
+
 public struct NetworkPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public func loadFromJson<T: Decodable>(file: URL) -> T {
+        loadFromFile(file)
     }
 }

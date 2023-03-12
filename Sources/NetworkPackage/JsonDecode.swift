1c194fdff8b5533a8aab0c54ecbaa6d74c4f43c7
antonio.virgone <antonio.virgone@wisemotions.com>
2023-03-12 17:58:26 +0100

    add json decode

---
+++ Sources/NetworkPackage/JsonDecode.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Antonio Virgone on 12/03/23.
+//
+
+import Foundation
+
+func loadFromFile<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    print(file)
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}

74fd89138d8636ad6972971b593b38c754105dd2
antonio.virgone <antonio.virgone@wisemotions.com>
2023-03-12 18:04:47 +0100

    wip

--- Sources/NetworkPackage/JsonDecode.swift
+++ Sources/NetworkPackage/JsonDecode.swift
@@ -7,23 +7,19 @@
 
 import Foundation
 
-func loadFromFile<T: Decodable>(_ filename: String) -> T {
+func loadFromFile<T: Decodable>(_ file: URL) -> T {
     let data: Data
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
-    }
     print(file)
     do {
         data = try Data(contentsOf: file)
     } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        fatalError("Couldn't load \(file) from main bundle:\n\(error)")
     }
 
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch {
-        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        fatalError("Couldn't parse \(file) as \(T.self):\n\(error)")
     }
 }
